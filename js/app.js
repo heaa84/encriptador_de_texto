fc474de031666e59458a5eaa83fb0e7bf1ee8f87
heaa84 <hugo.alarcon@icorp.com.mx>
2024-07-03 22:23:51 -0600

    implementacion de la funcion generar mensaje y correcion de nombre de variables

--- js/app.js
+++ js/app.js
@@ -1,48 +1,56 @@
-let textNormal= "";             /*Variable que ba a contener el texto original*/
-let textCodificado= "";         /*Variable para el texto cifrado*/
-let abecedarioMinusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ']
-let textEtatus= false;          /*Para saver si el texto va a estar cifrar = true o no = false*/ 
-let textViable= false;          /*Verifica si el mensaje tiene la letras del abecedario y men minusculas */
-let listaTextNormal=["e","i","a","o","u"];         /*Lista de palabras o letras a cifrar del mensaje*/
-let listaTextCodificación =["enter","imes","ai","ober","ufat"];   /*Lista cifrado de las palabras*/
-let mensaje_ejemplo="mensaje normal aeiou"; /*cuando de termine el progranma se borrara*/
-let mensaje_ejemplo_cifrado="aienterimesoberufat";
-/*Ocultar botón de copiar*/
+let mensaje = "mensaje prueba";             /*Variable que va a contener el mensaje original*/
+let mensajeC = "";         /*Variable para el texto cifrado*/
+let mensajeN="";/**mensaje generado */
+let abcMinusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+let textStatus = false;          /*Para saver si el texto va a estar cifrar = true o no = false*/
+let textViable = false;          /*Verifica si el mensaje tiene la letras del abecedario y men minusculas */
+let listL = ["e", "i", "a", "o", "u"];         /*Lista de letras a cifrar del mensaje*/
+let listP = ["enter", "imes", "ai", "ober", "ufat"];   /*Lista de palabras a cifrar del mensaje*/
 
 
-function evalua_mensaje(mensaje_ejemplo,textEtatus,textViable,abecedarioMinusculas) {
-/* en este ciclo for verificamos que se cumpla que este en minusculas y sin caracteres especiales */
+
+function evalua_mensaje(mensaje_ejemplo, textStatus, textViable, abcMinusculas) {
+    /* en este ciclo for verificamos que se cumpla que este en minusculas y sin caracteres especiales */
     for (let indexCadena = 0; indexCadena < mensaje_ejemplo.length; indexCadena++) {
-        for (let indexList = 0; indexList < abecedarioMinusculas.length; indexList++) {
+        for (let indexList = 0; indexList < abcMinusculas.length; indexList++) {
             console.log(`INDICE CADENA ${indexCadena} -- INDICE LISTA ${indexList}`)
-            if (mensaje_ejemplo[indexCadena]==abecedarioMinusculas[indexList]) {
-                textViable=true;
+            if (mensaje_ejemplo[indexCadena] == abcMinusculas[indexList]) {
+                textViable = true;
                 break;
             }
         }
     }
 
-// Usar el método some para verificar si alguna palabrade la lista está en la cadena
-    textEtatus = listaTextCodificación.some(palabra => mensaje_ejemplo.includes(palabra));
-    console.log(`mensaje codificado ${textEtatus}`);
+    // Usar el método some para verificar si alguna palabrade la lista está en la cadena
+    textStatus = listP.some(palabra => mensaje_ejemplo.includes(palabra));
+    console.log(`mensaje codificado ${textStatus}`);
     console.log(`mensaje viable ${textViable}`);
 }
 
-function cifraDecifraMensaje(listaTextNormal,listaTextCodificación,textViable,textEtatus) {
-/*si mensaje esta cifrado*/
-    if (textViable==true || textEtatus==true) {
-        let mensaje=
-        console.log("imprime mensaje normal");
-    } if (textViable==true || textEtatus==false) { /*si mensaje no esta cifrado*/
-        console.log("imprime mensaje cifrado del lado derecho");
-    } else {
-        console.log("error");
+function generar_mensaje(mensaje,listL, listP, mensajeN) {
+    for (let indexM = 0; indexM < mensaje.length; indexM++) {/**indexM indice de mensaje */
+        for (let indexL = 0; indexL < listL.length; indexL++) {/**indexL indice de las listas */
+            if (mensaje[indexM]==listL[indexL]) {
+                mensajeN=mensajeN+listP[indexL];
+                console.log(mensaje);
+                console.log(mensajeN);
+                indexM++;
+                break;       
+            }
+        }
+        if (indexM < mensaje.length) {
+            mensajeN=mensajeN+mensaje[indexM];
+            console.log(indexM);
+        }
     }
-
-    
+    return(mensajeN);
 }
 
 console.log(`mensaje viable ${textViable}`);
-console.log(`mensaje codificado ${textEtatus}`);
-evalua_mensaje(mensaje_ejemplo,textEtatus,textViable,abecedarioMinusculas);
-cifraDecifraMensaje(listaTextNormal,listaTextCodificación,textViable,textEtatus)
+console.log(`mensaje codificado ${textStatus}`);
+evalua_mensaje(mensaje, textStatus, textViable, abcMinusculas);
+if (!textStatus) {
+    mensajeC=generar_mensaje(mensaje,listL, listP, mensajeN)/** codificamos el emsage y lo guardamos en mensajeC */
+}
+
+console.log(mensajeC);
